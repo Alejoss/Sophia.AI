acd7e9de3d1ba0ba049f8a28c6811633bac4a1b0
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-03 14:55:50 -0500

    Moving some files.

--- content/utils.py
+++
@@ -1,8 +0,0 @@
-from .models import File
-from django import forms
-
-
-class FileUploadForm(forms.ModelForm):
-    class Meta:
-        model = File
-        fields = ['file', 'title']
--- profiles/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from profiles.models import Profile, AcceptedCrypto, ContactMethod, CryptoCurrency
-
-admin.site.register(Profile)
-admin.site.register(AcceptedCrypto)
-admin.site.register(ContactMethod)
-admin.site.register(CryptoCurrency)
