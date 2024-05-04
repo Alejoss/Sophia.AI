e97059d1958029702a663d56acd050b3469a82a3
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-03 21:45:25 -0500

    Fixing the ganache launching command.

--- contracts/truffle-launch.sh
+++ contracts/truffle-launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASE="$(dirname "$0")"
 mkdir -p $BASE/local/db
-ganache-cli --mnemonic "dentist whale pattern drastic time black cigar bike person destroy punch hungry" --accounts 100 --defaultBalanceEther 1000 --db "$BASE/local/db"
+ganache-cli --wallet.mnemonic "dentist whale pattern drastic time black cigar bike person destroy punch hungry" --wallet.totalAccounts 100 --wallet.defaultBalance 1000 --db "$BASE/local/db"
