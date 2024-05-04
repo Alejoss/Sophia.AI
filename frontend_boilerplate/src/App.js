939ead71429642a72c2d818f30b43ea141c369cf
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-04 17:42:08 -0500

    Properly fixing the env. variables.

--- frontend_boilerplate/src/App.js
+++ frontend_boilerplate/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "./Content/Main";
 
 function App() {
+  let chainId = parseInt(process.env.REACT_APP_CHAIN_ID);
+  let chainName = process.env.REACT_APP_CHAIN_NAME;
+  console.log(`Launching app for chain-id=[${chainId}] chain-name=[${chainName}]`)
   return (
     <Wrapper expectedChainId={80002} expectedChainName={"Polygon Amoy"}>
       <BrowserRouter>
