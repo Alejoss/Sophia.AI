3952f66c491ba5d0977592fba9c787e71b6c6349
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-07 16:41:23 -0500

    Adding the label's default props.

--- frontend_boilerplate/src/Content/Label.js
+++ frontend_boilerplate/src/Content/Label.js
@@ -4,7 +4,7 @@ const defaultSx = {p: 2, display: 'inline-block'};
 
 function Label({ sx, children, ...props }) {
     sx = {...defaultSx, ...sx};
-    return <Typography sx={sx}>{children}</Typography>
+    return <Typography sx={sx} {...props}>{children}</Typography>
 }
 
 export default Label;
