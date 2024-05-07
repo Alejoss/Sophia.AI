7b43df1901d849984ff38abc7817cdee33bc8a11
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-07 16:28:56 -0500

    Changing inline to inline-block in the label.

--- params_frontend/src/Content/Label.js
+++ params_frontend/src/Content/Label.js
@@ -1,6 +1,6 @@
 import {Typography} from "@mui/material";
 
-const defaultSx = {p: 2, display: 'inline'};
+const defaultSx = {p: 2, display: 'inline-block'};
 
 function Label({ sx, children, ...props }) {
     sx = {...defaultSx, ...sx};
