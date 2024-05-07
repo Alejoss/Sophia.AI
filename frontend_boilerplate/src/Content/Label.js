a64cb63c3251c222d4273860680ed8bd872a564b
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-07 16:58:14 -0500

    Fixing the label to work as a field label.

--- frontend_boilerplate/src/Content/Label.js
+++ frontend_boilerplate/src/Content/Label.js
@@ -1,6 +1,6 @@
 import {Typography} from "@mui/material";
 
-const defaultSx = {p: 2, display: 'inline-block'};
+const defaultSx = {p: 2, display: 'block', textAlign: 'right'};
 
 function Label({ sx, children, ...props }) {
     sx = {...defaultSx, ...sx};
