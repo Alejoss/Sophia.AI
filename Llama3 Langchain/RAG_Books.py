b8cb9f8d570fa049a49148c7eaaea2fa57a8612f
Alejandro_PC <alejoveintimilla@gmail.com>
2024-05-09 18:53:55 -0500

    langchain for RAG

--- Llama3 Langchain/RAG_Books.py
+++ Llama3 Langchain/RAG_Books.py
@@ -1,7 +1,20 @@
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
+import os
+import shutil
+
+CHROMA_PATH = "chroma"
 DATA_PATH = "/books"
+
+
+def generate_data_store():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
 
 
 # https://github.com/mlschmitt/classic-books-markdown/tree/main
@@ -11,14 +24,31 @@ def load_documents():
     return documents
 
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=500,
-    length_function=len,
-    add_start_index=True
-)
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"{len(documents)} documents split into {len(chunks)} chunks")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
 
 
-def generate_data_store():
-    documents = load_documents()
-    chunks = text_splitter(documents)
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Create a new DB from the documents.
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
