3156e9052b8860c9d446950698ddda8d5ea65301
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-12 18:30:19 -0500

    Adding the hardhat contract sizer plugin.

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition-ethers");
+require("hardhat-contract-sizer");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
