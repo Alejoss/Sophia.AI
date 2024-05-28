efbb1044e7e2491866b3e875a05b395e7eb57b03
Alejandro Veintimilla <alejoveintimilla@gmail.com>
2024-05-27 20:39:35 -0500

    remove unused variable

--- contracts/contracts/ACBCToken.sol
+++ contracts/contracts/ACBCToken.sol
@@ -5,13 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, Ownable, ERC20Permit {
+contract ACBCToken is ERC20, Ownable, ERC20Permit {
 
     uint256 public constant INITIAL_SUPPLY = 1000000 * 10 ** 18;
 
     constructor(
-        address initialOwner,
-        uint256 initialSupply
+        address initialOwner
     )
         ERC20("ACBCToken", "ACBC")
         Ownable(initialOwner)
@@ -39,7 +38,3 @@ contract MyToken is ERC20, Ownable, ERC20Permit {
         _burn(account, amount);
     }
 }
-
-
-// 0x0Fa50B058e9CCAb5AfD2337F0dF997801715a66e
-
