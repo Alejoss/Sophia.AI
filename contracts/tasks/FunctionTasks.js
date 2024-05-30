1fc1cb02ccd6b2bce5a15260fafa465541c9bd4d
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-29 22:06:19 -0500

    Adding the task and the utils function.

---
+++ contracts/tasks/FunctionTasks.js
@@ -0,0 +1,21 @@
+const { runSandboxCode } = require("../utils/chainlinkFunctionUtils");
+const { task } = require("hardhat/config");
+
+
+task("deploy-everything", "Deploys all our ecosystem")
+    .addParam("sourceFilename", "The file whose code will be run")
+    .addOptionalParam("configFilename", "The file with extra config (args, bytesArgs, secrets)")
+    .setAction(async ({ sourceFilename, configFilename }, hre, runSuper) => {
+        if (!sourceFilename) {
+            console.error("The source file is unspecified");
+            return;
+        }
+
+        try
+        {
+            console.log(await runSandboxCode(sourceFilename, configFilename));
+        } catch(e)
+        {
+            console.error(e);
+        }
+    });
---
+++ contracts/utils/chainlinkFunctionUtils.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { simulateScript } = require("@chainlink/functions-toolkit");
+
+
+async function runSandboxCode(sourceFilename, configFilename) {
+    const source = fs.readFileSync(sourceFilename, 'utf8');
+    if (!source)
+    {
+        throw new Error("No source file contents");
+    }
+
+    let config = {args:[], bytesArgs:[], secrets:{}};
+    try
+    {
+        if (configFilename) {
+            config = JSON.parse(fs.readFileSync(configFilename, 'utf8'));
+            config = {args: config.args || [], bytesArgs: config.bytesArgs || [], secrets: config.secrets || {}};
+        }
+    }
+    catch(e)
+    {
+        console.error("Error loading the config file (no config will be used):");
+        throw e;
+    }
+
+    return await simulateScript({
+        "source": source, ...config
+    });
+}
