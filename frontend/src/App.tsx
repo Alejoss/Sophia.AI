54bf9422f42684351c6955fe5bd8cd7feb3e647a
Alejandro_PC <alejoveintimilla@gmail.com>
2024-06-20 16:33:21 -0500

    frontend settup. React.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+};
+
+export default App;
