72d132fc150e3fd0d3c4a6acb743f6953442929f
Alejandro_PC <alejoveintimilla@gmail.com>
2024-06-24 15:52:33 -0500

    main.tsx setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App.tsx'
-import './index.css'
+import App from './App';
+import './index.css';
 
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-       </Provider>
-    </React.StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
