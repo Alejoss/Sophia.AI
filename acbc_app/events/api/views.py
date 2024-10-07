5fbd92b6d006f7749ed0b6ff2da8c53a1d5cb701
Alejandro_PC <alejoveintimilla@gmail.com>
2024-10-07 16:34:20 -0500

    fix:correct imports

--- acbc_app/events/api/views.py
+++ acbc_app/events/api/views.py
@@ -4,47 +4,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 
-from events.models import ConnectionPlatform, Event, Bookmark, Comment, CertificateRequest, Certificate
+from events.models import Event
+from comments.models import Comment
+from certificates.models import Certificate, CertificateRequest
 from events.api.serializers import (ConnectionPlatformSerializer, EventSerializer,
                           BookmarkSerializer, CommentSerializer, CertificateRequestSerializer,
                           CertificateSerializer)
 
-
-class ConnectionPlatformList(APIView):
-    def get(self, request, format=None):
-        platforms = ConnectionPlatform.objects.all()
-        serializer = ConnectionPlatformSerializer(platforms, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ConnectionPlatformSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ConnectionPlatformDetail(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(ConnectionPlatform, pk=pk)
-
-    def get(self, request, pk, format=None):
-        platform = self.get_object(pk)
-        serializer = ConnectionPlatformSerializer(platform)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        platform = self.get_object(pk)
-        serializer = ConnectionPlatformSerializer(platform, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        platform = self.get_object(pk)
-        platform.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class EventList(APIView):
@@ -157,43 +123,43 @@ class CertificateDetail(APIView):
         certificate.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+#
+# class BookmarkList(APIView):
+#     def get(self, request, format=None):
+#         bookmarks = Bookmark.objects.filter(user=request.user)
+#         serializer = BookmarkSerializer(bookmarks, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = BookmarkSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class BookmarkList(APIView):
-    def get(self, request, format=None):
-        bookmarks = Bookmark.objects.filter(user=request.user)
-        serializer = BookmarkSerializer(bookmarks, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = BookmarkSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class BookmarkDetail(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Bookmark, pk=pk)
-
-    def get(self, request, pk, format=None):
-        bookmark = self.get_object(pk)
-        serializer = BookmarkSerializer(bookmark)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        bookmark = self.get_object(pk)
-        serializer = BookmarkSerializer(bookmark, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        bookmark = self.get_object(pk)
-        bookmark.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+#
+# class BookmarkDetail(APIView):
+#     def get_object(self, pk):
+#         return get_object_or_404(Bookmark, pk=pk)
+#
+#     def get(self, request, pk, format=None):
+#         bookmark = self.get_object(pk)
+#         serializer = BookmarkSerializer(bookmark)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         bookmark = self.get_object(pk)
+#         serializer = BookmarkSerializer(bookmark, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         bookmark = self.get_object(pk)
+#         bookmark.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
 
 class CertificateRequestList(APIView):
     def get(self, request, format=None):
