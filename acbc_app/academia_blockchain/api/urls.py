b326f63c185cb4dd41dc04062a22c93af80850d3
Alejandro_PC <alejoveintimilla@gmail.com>
2024-10-07 16:37:08 -0500

    fix:correct imports

--- acbc_app/academia_blockchain/api/urls.py
+++ acbc_app/academia_blockchain/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from profiles.api.views import (ProfileList, ProfileDetail, UserDetailView, UserListView,
+from profiles.api.views import (ProfileList, ProfileDetail, UserDetailView,
                                 UserProfileView, Logout, Login, GetCsrfToken)
 from events.api.views import EventList, EventDetail
-from content.api.views import (LibraryList, LibraryDetail, GroupList, GroupDetail, FileList,
-                               FileDetail)
+# from content.api.views import (LibraryList, LibraryDetail, GroupList, GroupDetail, FileList,
+#                                FileDetail)
 
 
 urlpatterns = [
@@ -14,17 +14,14 @@ urlpatterns = [
     path('profiles/get_csrf_token/', GetCsrfToken.as_view(), name='get_csrf_token'),
     path('profiles/login/', Login.as_view(), name='login'),
     path('profiles/logout/', Logout.as_view(), name='logout'),
-    path('users/', UserListView.as_view(), name='user-detail'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='file-detail'),
 
     path('events/', EventList.as_view(), name='event-list'),
     path('events/<int:pk>/', EventDetail.as_view(), name='event-detail'),
-    path('libraries/', LibraryList.as_view(), name='library-list'),
-    path('libraries/<int:pk>/', LibraryDetail.as_view(), name='library-detail'),
-    path('groups/', GroupList.as_view(), name='group-list'),
-    path('groups/<int:pk>/', GroupDetail.as_view(), name='group-detail'),
-    path('files/', FileList.as_view(), name='file-list'),
-    path('files/<int:pk>/', FileDetail.as_view(), name='file-detail'),
-
-
+    # path('libraries/', LibraryList.as_view(), name='library-list'),
+    # path('libraries/<int:pk>/', LibraryDetail.as_view(), name='library-detail'),
+    # path('groups/', GroupList.as_view(), name='group-list'),
+    # path('groups/<int:pk>/', GroupDetail.as_view(), name='group-detail'),
+    # path('files/', FileList.as_view(), name='file-list'),
+    # path('files/<int:pk>/', FileDetail.as_view(), name='file-detail'),
 ]
