9038ca299b149371a34ccdae3fb02bfa9e364b01
linyers <linyers666@gmail.com>
2024-10-11 16:05:17 -0300

    add: author field in KnowledgePath model

--- acbc_app/content/models.py
+++ acbc_app/content/models.py
@@ -77,6 +77,7 @@ class KnowledgePath(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_paths')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
